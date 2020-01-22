680b244ee1ddd3efc8e85099f54087fd1955838e
Shubham Dixit <sdixit@grepruby.io>
2020-01-22 12:15:44 +0530

    Added Idea how to implement useState with functions

--- src/counter/index.jsx
+++ src/counter/index.jsx
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 const Counter = () => {
     const initialState = 0
     const [ count, setCount ] = useState( initialState )
+
+    const incrementByTen = () => {
+        return ( count + 10 )
+    }
     return (
     <div>
         Count: 
@@ -10,6 +14,7 @@ const Counter = () => {
         <button onClick={ () => setCount( count + 1) }>+</button>
         <button onClick={ () => setCount( count - 1) }>-</button>
         <button onClick={ () => setCount( initialState) }>Reset</button>
+        <button onClick={ () => setCount( incrementByTen) }>Increment by 10</button>
     </div>
     )
  }
