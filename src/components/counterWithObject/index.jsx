65fdc6673c0aa1fe44aca2cc9f12099f58905c47
Shubham Dixit <sdixit@grepruby.io>
2020-01-22 13:36:10 +0530

    Added Counter With Object

--- src/components/counterWithObject/index.jsx
+++ src/components/counterWithObject/index.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 
 const ObjectCounter = () => {
-    const [ name, setName ] = useState({ firstName: 'Tom', lastName: 'John' } )
-
-    const setNameRandom = () => {
-        setName( prevName => ( { firstName: 'New',  lastName: 'Name' } ) )
-    }
+    const [ name, setName ] = useState({ firstName: '', lastName: '' } )
 
     return (
     <div>
-        Object : { name.firstName + ' ' + name.lastName }
-        <button onClick = { setNameRandom }> Set Name </button>
+        Your Name : { name.firstName + ' ' + name.lastName }
+        <input
+         type = "text"
+         value={ name.firstName }
+         onChange={ e => setName( { ...name, firstName: e.target.value } ) }
+        />
+        <input
+         type = "text"
+         value={ name.lastName }
+         onChange={ e => setName( {  ...name, lastName: e.target.value } ) }
+        />
     </div>
     )
  }
